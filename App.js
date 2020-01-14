e814e9285026fde99e25301676f0490f91733299
zedonora <timeundtime@gmail.com>
2020-01-15 05:14:56 +0900

    log in & log out

--- App.js
+++ App.js
@@ -3,17 +3,44 @@ import { Ionicons } from "@expo/vector-icons";
 import { AppLoading } from "expo";
 import * as Font from "expo-font";
 import { Asset } from "expo-asset";
-import { Text, View } from "react-native";
+import { AsyncStorage } from "react-native";
+import { InMemoryCache } from "apollo-cache-inmemory";
+import { persistCache } from "apollo-cache-persist";
+import ApolloClient from "apollo-boost";
+import { ThemeProvider } from "styled-components";
+import { ApolloProvider } from "react-apollo-hooks";
+import apolloClientOptions from "./apollo";
+import styles from "./styles";
+import NavController from "./components/NavController";
+import { AuthProvider } from "./AuthContext";
 
 export default function App() {
 	const [loaded, setLoaded] = useState(false);
+	const [client, setClient] = useState(null);
+	const [isLoggedIn, setIsLoggedIn] = useState(null);
 	const preLoad = async () => {
 		try {
 			await Font.loadAsync({
 				...Ionicons.font
 			});
 			await Asset.loadAsync([require("./assets/logo.png")]);
+			const cache = new InMemoryCache();
+			await persistCache({
+				cache,
+				storage: AsyncStorage
+			});
+			const client = new ApolloClient({
+				cache,
+				...apolloClientOptions
+			});
+			const isLoggedIn = await AsyncStorage.getItem("isLoggedIn");
+			if (!isLoggedIn || isLoggedIn === "false") {
+				setIsLoggedIn(false);
+			} else {
+				setIsLoggedIn(true);
+			}
 			setLoaded(true);
+			setClient(client);
 		} catch (e) {
 			console.log(e);
 		}
@@ -21,10 +48,15 @@ export default function App() {
 	useEffect(() => {
 		preLoad();
 	}, []);
-	return loaded ? (
-		<View>
-			<Text>Open up App.js to start working on your app!</Text>
-		</View>
+
+	return loaded && client && isLoggedIn !== null ? (
+		<ApolloProvider client={client}>
+			<ThemeProvider theme={styles}>
+				<AuthProvider isLoggedIn={isLoggedIn}>
+					<NavController />
+				</AuthProvider>
+			</ThemeProvider>
+		</ApolloProvider>
 	) : (
 		<AppLoading />
 	);
---
+++ apollo.js
@@ -0,0 +1,4 @@
+const apolloClientOptions = {
+	uri: "http://localhost:4000/graphql"
+};
+export default apolloClientOptions;
